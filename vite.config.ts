86dae978600dbec86e471eb545a5d5b958efe470
Harshal Bhangale <buddyharshal2751@gmail.com>
2025-07-31 15:19:17 +0530

    fix: configure Vite to resolve @ paths using vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'; // ✅ add this
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(), // ✅ add this
+  ],
+});
